5487ce41a4c38bf57e5453d4decc77dc97ff178f
achidambaram17 <47485527+adityachidambaram@users.noreply.github.com>
2022-01-21 16:51:35 -0500

    b

--- src/globals.cpp
+++ src/globals.cpp
@@ -9,8 +9,8 @@ pros::Motor frontLeft(9, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_
 pros::Motor lift(1, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_ROTATIONS);
 //pros::Motor clamp(10, pros::E_MOTOR_GEARSET_18, true, pros::E_MOTOR_ENCODER_DEGREES);
 
-pros::Motor rightWing(3, pros::E_MOTOR_GEARSET_18, true, pros::E_MOTOR_ENCODER_ROTATIONS);
-pros::Motor leftWing(10, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_ROTATIONS);
+pros::Motor rightWing(6, pros::E_MOTOR_GEARSET_36, true, pros::E_MOTOR_ENCODER_ROTATIONS);
+pros::Motor leftWing(10, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_ROTATIONS);
 
 //Piston
 #define DIGITAL_SENSOR_PORT 'A'
